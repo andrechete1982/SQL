61c6bb3c877163891f202612fa89634376e0f4b1
andrechete1982 <andrescamargor1982@gmail.com>
2026-03-03 07:16:50 -0500

    Update full_confo_querie.sql

--- full_confo_querie.sql
+++ full_confo_querie.sql
@@ -1,12 +1,24 @@
---We are missing just to add the RPN rule and match it with the Brand Classf file 
+CREATE OR REPLACE TABLE `personal-marketing-analytics.mart.conversions_granular`
+PARTITION BY date
+CLUSTER BY marsha_code, media_buy_key
+AS
 
 WITH base AS (
 
   SELECT
     date,
+    campaign, #Campaign friendly name
+    campaign_id, #Campaign id
+    activity, #Floodlight Activity
+    activity_id, #Floodlight Activity ID
     placement AS media_buy_name,
-    placement_id AS media_buy_key, 
+    placement_id AS media_buy_key,
+    marsha_code_string_ AS marsha_code,
+    mr_prof_member_program_level_string_ AS loyalty_tier,
+    checkin_date_string_ AS check_in,
+    departure_date_or_check_out_date_string_ as check_out,  
 
+    room_nights_string_ AS rn,
     click_through_conversions AS ctc,
     view_through_conversions  AS vtc,
     click_through_revenue     AS ctr,
@@ -39,9 +51,12 @@ enriched AS (
     pub.description AS site,
     ch.description  AS channel,
     mi.description  AS brand_display,
-    sl.description  AS special_activation
+    sl.description  AS special_activation,
+    br.brand AS hotel_brand
 
   FROM base b
+
+  -- Taxonomy Joins 
 
   LEFT JOIN `personal-marketing-analytics.Clean_data.taxo_dict_clean` jp
     ON b.jp_calc = jp.code
@@ -63,38 +78,44 @@ enriched AS (
     ON b.special_activation_calc = sl.code
    AND sl.delimiter = 'SL~'
 
+  -- Marsha Dimension Joins
+
+  LEFT JOIN `personal-marketing-analytics.Clean_data.plu_file_clean` br
+    ON b.marsha_code = br.marsha_code
+
 ),
 
 attribution_logic AS (
 
-  SELECT
+SELECT
     *,
 
-    -- Weight logic (centralized)
+    -- Centralized weight logic
     CASE
-      WHEN jp IN ('Browse','Shop','Buy') THEN 0.15
+      WHEN jp IN ('Browse','Shop') THEN 0.15
+      WHEN jp = 'Buy' THEN 0.07
       ELSE 0
     END AS view_weight,
 
     -- Attributed Revenue
     COALESCE(ctr,0)
-    +
-    COALESCE(vtr,0) *
-      CASE
-        WHEN jp IN ('Browse','Shop','Buy') THEN 0.15
-        ELSE 0
-      END
-    AS att_revenue,
+      + COALESCE(vtr,0) *
+        CASE
+          WHEN jp IN ('Browse','Shop') THEN 0.15
+          WHEN jp = 'Buy' THEN 0.07
+          ELSE 0
+        END
+      AS gross_att_revenue,
 
     -- Attributed Bookings
     COALESCE(ctc,0)
-    +
-    COALESCE(vtc,0) *
-      CASE
-        WHEN jp IN ('Browse','Shop','Buy') THEN 0.15
-        ELSE 0
-      END
-    AS att_bookings,
+      + COALESCE(vtc,0) *
+        CASE
+          WHEN jp IN ('Browse','Shop') THEN 0.15
+          WHEN jp = 'Buy' THEN 0.07
+          ELSE 0
+        END
+      AS gross_att_bookings,
 
     -- Attributed Room Nights
     CASE 
@@ -108,14 +129,15 @@ attribution_logic AS (
       THEN COALESCE(room_nights,0)
       ELSE 0
     END
-    *
-      CASE
-        WHEN jp IN ('Browse','Shop','Buy') THEN 0.15
-        ELSE 0
-      END
-    AS att_room_nights
+      *
+        CASE
+          WHEN jp IN ('Browse','Shop') THEN 0.15
+          WHEN jp = 'Buy' THEN 0.07
+          ELSE 0
+        END
+    AS gross_att_room_nights
 
-  FROM enriched
+FROM enriched
 
 )
 
