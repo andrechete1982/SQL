4479ddf0bc274a5cd7addb18ea0a993f0f0becb0
andrechete1982 <andrescamargor1982@gmail.com>
2026-03-04 16:44:59 -0500

    Update full_confo_querie.sql

--- full_confo_querie.sql
+++ full_confo_querie.sql
@@ -5,20 +5,25 @@ AS
 
 WITH base AS (
 
-  SELECT
+SELECT
     date,
-    campaign, #Campaign friendly name
-    campaign_id, #Campaign id
-    activity, #Floodlight Activity
-    activity_id, #Floodlight Activity ID
+    campaign,
+    campaign_id,
+    activity,
+    activity_id,
+
     placement AS media_buy_name,
     placement_id AS media_buy_key,
+
+    brand_string_ AS brand_ac,
     marsha_code_string_ AS marsha_code,
+
     mr_prof_member_program_level_string_ AS loyalty_tier,
     checkin_date_string_ AS check_in,
-    departure_date_or_check_out_date_string_ as check_out,  
+    departure_date_or_check_out_date_string_ AS check_out,
 
     room_nights_string_ AS rn,
+
     click_through_conversions AS ctc,
     view_through_conversions  AS vtc,
     click_through_revenue     AS ctr,
@@ -26,7 +31,6 @@ WITH base AS (
 
     floodlight_attribution_type AS fatp,
 
-    -- Cast once here (important)
     SAFE_CAST(room_nights_string_ AS FLOAT64) AS room_nights,
 
     REGEXP_EXTRACT(placement, r'JP~([^_]+)')  AS jp_calc,
@@ -35,16 +39,15 @@ WITH base AS (
     REGEXP_EXTRACT(placement, r'MI~([^_]+)')  AS brand_display_calc,
     REGEXP_EXTRACT(placement, r'SL~([^_]+)')  AS special_activation_calc
 
-  FROM `personal-marketing-analytics.Clean_data.confo_conversions_clean`
-  WHERE campaign_id = 34730231
-    AND date BETWEEN '2026-02-01' AND '2026-02-28'
-    AND site_cm360_ != 'APEX OUTCOMES (APEXOUTC)'
+FROM `personal-marketing-analytics.Clean_data.confo_conversions_clean`
+
+WHERE site_cm360_ != 'APEX OUTCOMES (APEXOUTC)'
 
 ),
 
 enriched AS (
 
-  SELECT
+SELECT
     b.*,
 
     jp.description  AS jp,
@@ -52,36 +55,33 @@ enriched AS (
     ch.description  AS channel,
     mi.description  AS brand_display,
     sl.description  AS special_activation,
+
     br.brand AS hotel_brand
 
-  FROM base b
+FROM base b
 
-  -- Taxonomy Joins 
+LEFT JOIN `personal-marketing-analytics.Clean_data.taxo_dict_clean` jp
+ON b.jp_calc = jp.code
+AND jp.delimiter = 'JP~'
 
-  LEFT JOIN `personal-marketing-analytics.Clean_data.taxo_dict_clean` jp
-    ON b.jp_calc = jp.code
-   AND jp.delimiter = 'JP~'
+LEFT JOIN `personal-marketing-analytics.Clean_data.taxo_dict_clean` pub
+ON b.site_calc = pub.code
+AND pub.delimiter = 'PUB~'
 
-  LEFT JOIN `personal-marketing-analytics.Clean_data.taxo_dict_clean` pub
-    ON b.site_calc = pub.code
-   AND pub.delimiter = 'PUB~'
+LEFT JOIN `personal-marketing-analytics.Clean_data.taxo_dict_clean` ch
+ON b.channel_calc = ch.code
+AND ch.delimiter = 'CH~'
 
-  LEFT JOIN `personal-marketing-analytics.Clean_data.taxo_dict_clean` ch
-    ON b.channel_calc = ch.code
-   AND ch.delimiter = 'CH~'
+LEFT JOIN `personal-marketing-analytics.Clean_data.taxo_dict_clean` mi
+ON b.brand_display_calc = mi.code
+AND mi.delimiter = 'MI~'
 
-  LEFT JOIN `personal-marketing-analytics.Clean_data.taxo_dict_clean` mi
-    ON b.brand_display_calc = mi.code
-   AND mi.delimiter = 'MI~'
+LEFT JOIN `personal-marketing-analytics.Clean_data.taxo_dict_clean` sl
+ON b.special_activation_calc = sl.code
+AND sl.delimiter = 'SL~'
 
-  LEFT JOIN `personal-marketing-analytics.Clean_data.taxo_dict_clean` sl
-    ON b.special_activation_calc = sl.code
-   AND sl.delimiter = 'SL~'
-
-  -- Marsha Dimension Joins
-
-  LEFT JOIN `personal-marketing-analytics.Clean_data.plu_file_clean` br
-    ON b.marsha_code = br.marsha_code
+LEFT JOIN `personal-marketing-analytics.Clean_data.plu_file_clean` br
+ON b.marsha_code = br.marsha_code
 
 ),
 
@@ -90,56 +90,92 @@ attribution_logic AS (
 SELECT
     *,
 
-    -- Centralized weight logic
     CASE
-      WHEN jp IN ('Browse','Shop') THEN 0.15
-      WHEN jp = 'Buy' THEN 0.07
-      ELSE 0
+        WHEN jp IN ('Browse','Shop') THEN 0.15
+        WHEN jp = 'Buy' THEN 0.07
+        ELSE 0
     END AS view_weight,
 
-    -- Attributed Revenue
     COALESCE(ctr,0)
-      + COALESCE(vtr,0) *
+    + COALESCE(vtr,0) *
         CASE
-          WHEN jp IN ('Browse','Shop') THEN 0.15
-          WHEN jp = 'Buy' THEN 0.07
-          ELSE 0
+            WHEN jp IN ('Browse','Shop') THEN 0.15
+            WHEN jp = 'Buy' THEN 0.07
+            ELSE 0
         END
-      AS gross_att_revenue,
+    AS gross_att_revenue,
 
-    -- Attributed Bookings
     COALESCE(ctc,0)
-      + COALESCE(vtc,0) *
+    + COALESCE(vtc,0) *
         CASE
-          WHEN jp IN ('Browse','Shop') THEN 0.15
-          WHEN jp = 'Buy' THEN 0.07
-          ELSE 0
+            WHEN jp IN ('Browse','Shop') THEN 0.15
+            WHEN jp = 'Buy' THEN 0.07
+            ELSE 0
         END
-      AS gross_att_bookings,
+    AS gross_att_bookings,
 
-    -- Attributed Room Nights
-    CASE 
-      WHEN fatp = 'Click-through'
-      THEN COALESCE(room_nights,0)
-      ELSE 0
+    CASE
+        WHEN fatp = 'Click-through'
+        THEN COALESCE(room_nights,0)
+        ELSE 0
     END
     +
-    CASE 
-      WHEN fatp = 'View-through'
-      THEN COALESCE(room_nights,0)
-      ELSE 0
-    END
-      *
+    CASE
+        WHEN fatp = 'View-through'
+        THEN COALESCE(room_nights,0)
+        ELSE 0
+    END *
         CASE
-          WHEN jp IN ('Browse','Shop') THEN 0.15
-          WHEN jp = 'Buy' THEN 0.07
-          ELSE 0
+            WHEN jp IN ('Browse','Shop') THEN 0.15
+            WHEN jp = 'Buy' THEN 0.07
+            ELSE 0
         END
     AS gross_att_room_nights
 
 FROM enriched
 
+),
+
+revenue_rule_applied AS (
+
+SELECT
+    a.*,
+
+    ROUND(
+        SAFE_DIVIDE(a.gross_att_revenue, NULLIF(a.gross_att_room_nights,0))
+    ,2) AS revenue_per_night,
+
+    r.invalid_threshold_rte,
+    r.substitute_rte,
+
+    ROUND(
+        CASE
+            WHEN a.gross_att_room_nights = 0 THEN 0
+            WHEN SAFE_DIVIDE(a.gross_att_revenue, NULLIF(a.gross_att_room_nights,0)) > r.invalid_threshold_rte
+            THEN r.substitute_rte * a.gross_att_room_nights
+            ELSE a.gross_att_revenue
+        END
+    ,2) AS adjusted_revenue,
+
+    CASE
+        WHEN SAFE_DIVIDE(a.gross_att_revenue, NULLIF(a.gross_att_room_nights,0)) > r.invalid_threshold_rte
+        THEN TRUE
+        ELSE FALSE
+    END AS revenue_rule_applied,
+
+    CASE
+        WHEN a.gross_att_room_nights = 0 THEN 'NO_NIGHTS'
+        WHEN SAFE_DIVIDE(a.gross_att_revenue, NULLIF(a.gross_att_room_nights,0)) > r.invalid_threshold_rte
+        THEN 'CAPPED'
+        ELSE 'VALID'
+    END AS revenue_rule_status
+
+FROM attribution_logic a
+
+LEFT JOIN `personal-marketing-analytics.raw_dv360.suspect_revenue_rates` r
+ON a.brand_ac = r.brand_cd
+
 )
 
 SELECT *
-FROM attribution_logic;
+FROM revenue_rule_applied
